5d3905f932defcc26cfd29144939726686feed94
Eustone Malaya <emalaya>
2021-01-02 18:22:11 +0200

    Changed test folder

---
+++ test/test_id_generator.py
@@ -0,0 +1,23 @@
+import unittest
+from uuid import uuid4
+from idgenerator import PackageID,CustomerID
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):pass
+    def test_package_id_generator(self):pass
+
+    def test_customer_id_generator(self):pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
+
+print(test_package_id_generator())
+
